e47c905df24a12627813a7ba0b366719c9f59e01
lijopj13 <49747597+lijopj13@users.noreply.github.com>
2022-06-24 13:34:13 +0530

    Update main.tf

--- exercise_4/main.tf
+++ exercise_4/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 terraform {
    backend "s3" {
-   bucket = "rc-tf-remote-state-bucket"
+   bucket = "lijo-remote-statefile-tf"
    key = "terraform.tfstate"
    region = "us-east-1"
   }
