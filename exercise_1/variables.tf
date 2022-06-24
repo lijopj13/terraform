2e96767063bae6c3696ce6e6f49f841bc09ea284
lijopj13 <49747597+lijopj13@users.noreply.github.com>
2022-06-24 11:17:49 +0530

    Update variables.tf

--- exercise_1/variables.tf
+++ exercise_1/variables.tf
@@ -16,7 +16,7 @@ variable "s3_bucket_region" {
 variable "s3_bucket_name" {
   type        = string
   description = "Name of the S3 bucket to be created used to store the remote state file"
-  default     = "lijo-terraform-s3
+  default     = "lijo-terraform-s3"
   }
 
 variable "tag_env" {
